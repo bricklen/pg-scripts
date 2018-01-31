1b369a4a9d6b2b4357703e8843f053040817665a
Bricklen Anderson <banderson@xmatters.com>
2018-01-31 07:07:29 -0800

    Updated to use fallocate.

--- create_2gb_removable_file.sh
+++ create_2gb_removable_file.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-## Purpose: Create a 2GB file under $PGDATA/ and one under $PGDATA/pg_xlog/
-##    so that if the system unexpectedly fills up, at least you can buy yourself
-##    some breathing room, or at least, for as long as it takes for the space
-##    to be reclaimed...
+## Purpose: Create a 2GB file at $PGDATA/.. 
+##    so that if the system unexpectedly fills up, you can buy yourself
+##    some breathing room to clear up space.
 ##
 ## Usage:
-##   cd to the volume where you want to add the 2GB file
+#   cd to the volume where you want to add the 2GB file
+#
+#dd if=/dev/zero of=remove_me_when_space_is_needed bs=1024 count=2000000
 
-dd if=/dev/zero of=remove_me_when_space_is_needed bs=1024 count=2000000
+# To create a 2GB "removeme" file:
+dir=/path/above/$DATADIR
+file=remove_me_when_space_is_needed
+dfree=$(echo $(($(stat -f --format="%a*%S" "$dir")/10**6)))
+if [ "$dfree" -gt 10000 ]; then
+    ## At least 10GB free
+    if [ ! -e "${dir}/${file}" ]; then
+        fallocate -l 2G "${dir}/${file}"
+    fi
+fi
 
